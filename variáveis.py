c96cc0d8b8d336cb77b42931d8a323d96da45603
amanda-galter <amanda.lotitos@gmail.com>
2024-04-23 16:37:57 -0300

    criação variáveis.py

---
+++ variáveis.py
@@ -0,0 +1,13 @@
+nome="Amanda Gálter Lotito"
+idade="15,99"
+email="minegril@hotmial.com"
+telefone="+55 11765449834"
+nota="7"
+aprovado= True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
+
