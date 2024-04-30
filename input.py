de4622ec9e471627fe5750aea616ab5413fd5fcf
amanda-galter <amanda.lotitos@gmail.com>
2024-04-30 16:32:18 -0300

    Criação input.py

---
+++ input.py
@@ -0,0 +1,10 @@
+# Entrada de Dados
+aluno = input('digite o nome do aluno: ')
+nota1 = float(input('digite a primeira nota: '))
+nota2 = float(input('digite a segunda nota: '))
+nota3 = float(input('digite a terceira nota: '))
+falta = int(input('digite a quantidade de faltas: '))
+media = (nota1+nota2+nota3)/3
+print('aluno: ' + aluno)
+print('media: ' + str(media))
+print('falta(s): ' + str( falta))
