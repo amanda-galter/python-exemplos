912b473e31e5b6d55d55479b1ca7352d600f4889
amanda-galter <amanda.lotitos@gmail.com>
2024-05-02 16:30:59 -0300

    criação fstrings2.py

---
+++ fstrings2.py
@@ -0,0 +1,7 @@
+#formatação de strings com varias linhas
+escola = 'Senai'
+curso= 'Desenvolvimento de Sistemas'
+uc='Logica de Programação'
+print(f"Escola: {escola}\n"
+      f"Curso: {curso}\n"
+      f"Unidade Curricular: {uc}")
