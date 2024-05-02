7ee6e166e25a097b99cc2109ce38d7cec21c7d07
amanda-galter <amanda.lotitos@gmail.com>
2024-05-02 16:44:10 -0300

    atualização fspring2.py

--- fstrings2.py
+++ fstrings2.py
@@ -2,6 +2,10 @@
 escola = 'Senai'
 curso= 'Desenvolvimento de Sistemas'
 uc='Logica de Programação'
+matricula=34
+nota=8.999999
 print(f"Escola: {escola}\n"
       f"Curso: {curso}\n"
-      f"Unidade Curricular: {uc}")
+      f"Unidade Curricular: {uc}\n"
+      f"Matricula: {matricula:03d}\n"
+      f"nota: {nota:.2f}")
