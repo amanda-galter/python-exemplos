6633a9bc5c463a4c3767d433107de4b5c0cb6664
amanda-galter <amanda.lotitos@gmail.com>
2024-05-21 16:42:50 -0300

    listas

---
+++ listas.py
@@ -0,0 +1,6 @@
+aluno=['Amanda Gálter', 4, 10,False]
+print(f'nome:{aluno[0]} \nfaltas:{aluno[1]} \nmedia:{aluno[2]}')
+if aluno[3] == True:
+    print(f'aprovado: sim')
+else:
+    print(f'aprovado: não')
