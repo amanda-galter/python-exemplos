93534aaa31a7ba79ff97bf5d0d605479aede84b7
amanda-galter <amanda.lotitos@gmail.com>
2024-05-23 10:15:52 -0300

    listas_remover.py

---
+++ listas_remover.py
@@ -0,0 +1,14 @@
+alunos=['Adriana','João','Maria','pedro','Ana','Lucas','Mariana','Adriana']
+print(f'lista de alunos: {alunos}')
+#metodo remove()
+alunos.remove('Adriana')
+print(f'lista de alunos {alunos}')
+#metodo pop ()
+alunos.pop(2)
+print(f'lista de alunos: {alunos}')
+#função del
+del alunos [3]
+print(f'lista de alunos: {alunos}')
+#método clear
+alunos.clear()
+print(f'lista de alunos: {alunos}')
